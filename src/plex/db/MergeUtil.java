c47a7dd5c5813bb56a23040590257472e1f251e6
Michael Grossniklaus <michael.grossniklaus@uni-konstanz.de>
2017-08-10 12:45:50 +0200

    Initial commit

---
+++ src/plex/db/MergeUtil.java
@@ -0,0 +1,18 @@
+package plex.db;
+
+/**
+ * Utility to merge user settings from one Plex database into another.
+ * 
+ * @author Michael Grossniklaus &lt;michael.grossniklaus@uni-konstanz.de&gt;
+ */
+public class MergeUtil {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
